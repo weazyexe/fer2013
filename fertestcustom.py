93b93f0fa555d09b6c5e274a5bdfdec4056a9d8b
weazy <weazy.exe@gmail.com>
2020-01-03 17:20:38 +0300

    test on lfw and celeba datasets

--- fertestcustom.py
+++ fertestcustom.py
@@ -3,12 +3,12 @@ from __future__ import division
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.models import model_from_json
-import numpy
 import os
 import numpy as np
 import cv2
+import time
 
-#loading the model
+# loading the model
 json_file = open('fer.json', 'r')
 loaded_model_json = json_file.read()
 json_file.close()
@@ -17,30 +17,76 @@ loaded_model = model_from_json(loaded_model_json)
 loaded_model.load_weights("fer.h5")
 print("Loaded model from disk")
 
-#setting image resizing parameters
+# setting image resizing parameters
 WIDTH = 48
 HEIGHT = 48
-x=None
-y=None
+x = None
+y = None
 labels = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
 
-#loading image
-full_size_image = cv2.imread("test.jpg")
-print("Image Loaded")
-gray=cv2.cvtColor(full_size_image,cv2.COLOR_RGB2GRAY)
-face = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-faces = face.detectMultiScale(gray, 1.3  , 10)
 
-#detecting faces
-for (x, y, w, h) in faces:
+def process(directory, filename, logFileName):
+    log = open(logFileName, "a+")
+
+    # loading image
+    full_size_image = cv2.imread(directory + "\\" + filename)
+    print(filename + " loaded. Recognizing...")
+
+    start = time.time()
+    emotion = recognize(full_size_image)
+    end = time.time()
+    diff = end - start
+
+    res = str(diff) + "s /// " + filename + " /// " + emotion
+    print(res)
+    log.write(res + "\n")
+
+    log.close()
+
+
+def lfw():
+    counter = 0
+
+    # a+ arg: 'a' means 'append', + means create if file doesn't exist
+
+    directories = os.walk("C:\\Users\\weazy\\Desktop\\lfw")
+    for directory in directories:
+        if counter != 0:
+            for files in os.walk(directory[0]):
+                for file in files[2]:
+                    process(directory[0], file, "lfw_result.txt")
+        else:
+            counter += 1
+
+
+def celeba():
+    path = "C:\\Users\\weazy\\Desktop\\celeba"
+    directories = os.walk(path)
+
+    for directory, subdirs, files in directories:
+        for file in files:
+            process(path, file, "celeba_result.txt")
+
+
+def recognize(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    face = cv2.CascadeClassifier('haarcascade_frontalface_alt2.xml')
+    faces = face.detectMultiScale(gray, 1.3, 10)
+
+    emotion = ""
+    # detecting faces
+    for (x, y, w, h) in faces:
         roi_gray = gray[y:y + h, x:x + w]
         cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
-        cv2.normalize(cropped_img, cropped_img, alpha=0, beta=1, norm_type=cv2.NORM_L2, dtype=cv2.CV_32F)
-        cv2.rectangle(full_size_image, (x, y), (x + w, y + h), (0, 255, 0), 1)
-        #predicting the emotion
-        yhat= loaded_model.predict(cropped_img)
-        cv2.putText(full_size_image, labels[int(np.argmax(yhat))], (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 1, cv2.LINE_AA)
-        print("Emotion: "+labels[int(np.argmax(yhat))])
+        yhat = loaded_model.predict(cropped_img)
+        emotion = labels[int(np.argmax(yhat))]
 
-cv2.imshow('Emotion', full_size_image)
-cv2.waitKey()
+    return emotion
+
+
+dataset = input("Input dataset (0 - LFW / 1 - CelebA): ")
+
+if dataset == "0":
+    lfw()
+else:
+    celeba()
